0a3d6e3cd90d0a7d184e768b29af88dbdab120d6
Trevor Lister <Trevor Lister>
2022-02-23 07:06:24 -0300

    Renders other routes

--- app.js
+++ app.js
@@ -11,11 +11,35 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}))
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", {startingContent: homeStartingContent});
-})
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent})
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent})
+});
+
+app.get("/compose", (req, res) => {
+  const newComposition = req.body.newComposition;
+  console.log(newComposition)
+  res.render("compose", {publishComposition: newComposition})
+});
+
+app.post("/compose", (req, res) => {
+  const composition = req.body.newComposition;
+  console.log(composition)
+  res.render("compose")
+  
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
